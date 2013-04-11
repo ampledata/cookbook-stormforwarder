133d8b84c0dfc3c402e4bea2189241f3fdcb5cd8
Avrohom Katz <iambpentameter@gmail.com>
2013-04-11 14:38:50 -0400

    update existing monitor if it exists

--- recipes/enable.rb
+++ recipes/enable.rb
@@ -29,11 +29,16 @@ else
 end
 
 node['stormforwarder']['monitors'].each do |config_hash|
-  monitor_params = config_hash.map{|k,v| "-#{k} #{v}"}
+  monitor_params = config_hash.map{|k,v| "-#{k} #{v}"}.join(" ")
+  monitor_exists = "/opt/splunkforwarder/bin/splunk list monitor | sed -e 's/^[ \t]*//' | grep '^#{config_hash['source']}$'"
+
+  execute "update monitor for #{config_hash['source']}" do
+    command "/opt/splunkforwarder/bin/splunk edit monitor #{monitor_params}"
+    only_if  monitor_exists
+  end
 
   execute "add monitor for #{config_hash['source']}" do
     command "/opt/splunkforwarder/bin/splunk add monitor #{monitor_params}"
-
-    not_if  "/opt/splunkforwarder/bin/splunk list monitor | sed -e 's/^[ \t]*//' | grep '^#{config_hash['source']}$'"
+    not_if  monitor_exists
   end
 end
